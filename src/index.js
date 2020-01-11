8a08d347953db8661d15421c074bdd3859576ade
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-01-11 13:07:10 +0500

    sentry bug monitoring added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+import * as Sentry from "@sentry/browser";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://fb9eb9e78b9b47658e97db5490fc6e67@sentry.io/1878118"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
